088b0573e54f0ca2c492ae2ffcfa314881d37e7c
Edlyn Thorne <edlyn.thorne@gmail.com>
2024-08-04 14:22:38 +0400

    Remove the custom activity CSS since Discord fixed it

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-find src/ | entr -s 'lessc src/tweaks.less build/tweaks.css; cp build/tweaks.css ~/.config/Vencord/themes/'
+find src/ | entr -s 'lessc src/tweaks.less build/tweaks.css; cp build/tweaks.css ~/.config/vesktop/themes/'
